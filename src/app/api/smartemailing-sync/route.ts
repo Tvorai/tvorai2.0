3dd67de796fca0f855b03853175fad44611bc70d
Pato <infotvorai@gmail.com>
2026-04-02 13:38:40 +0200

    feat: set contact status to confirmed in smartemailing-sync

--- src/app/api/smartemailing-sync/route.ts
+++ src/app/api/smartemailing-sync/route.ts
@@ -28,6 +28,7 @@ export async function POST(req: NextRequest) {
         contactlists: [ 
           { 
             id: Number(listId), 
+            status: "confirmed", 
           }, 
         ], 
       }), 
